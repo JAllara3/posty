8bde4edadd3eaa5288ddc7ae870170270c7bad9e
Justin Allara <allara123@gmail.com>
2022-06-05 16:19:55 -0500

    style update and homepage updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            Home
+    <div class="flex justify-center justify-items-center">
+        <div class="w-10/12 bg-white p-6 rounded-lg">
+            <span>Laravel Blog!</span>
+        </div>
+    </div>
+
+    <div class="flex justify-center justify-items-center mt-2">
+        <div class="flex w-10/12 justify-center">
+            <div class="bg-white p-6 rounded-lg w-5/12 mr-2 ">
+                <h2 class="text-2xl">Current status</h2>
+                <p>This website allows people to register an account, and makes posts that others can like and comment on.</p>
+                <p>You can also delete and edit your own posts.</p>
+                <p>users can navigate to other users pages and view what they are talking about.</p>
+            </div>
+            <div class="bg-white p-6 rounded-lg w-5/12">
+                <h2 class="text-2xl">Updates in progress: </h2>
+                <p>create a place for users to upload profile pictures.</p>
+                <p>add the ability to tag your posts and allow users to filter posts by tag.</p>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <ul class="flex items-center">
                 @auth
                     <li>
-                        <a href="" class="p-3">{{ auth()->user()->username }}</a>
+                        <a href="/users/{{ auth()->user()->username }}/posts" class="p-3">{{ auth()->user()->username }}</a>
                     </li>
                     <li>
                         <form action="{{ route('logout') }}" method="post" class="inline p-3">
